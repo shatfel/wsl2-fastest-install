50fabeede2a4741bb78662a48c28943ecd7820c8
shatfel <shatfel@users.noreply.github.com>
2021-08-18 10:10:01 +0300

    Update wsl2-fastest-install.ps1
    fix requEst

--- wsl2-fastest-install.ps1
+++ wsl2-fastest-install.ps1
@@ -1,5 +1,5 @@
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-invoke-webrequst -uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi"
+invoke-webrequest -uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi"
 wsl_update_x64.msi
 wsl --set-default-version 2
